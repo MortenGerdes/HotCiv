4a3009324759d01b4261a4bdfd2fd2b37b8e61ef
Morten <mortengerdes7000@gmail.com>
2017-09-11 17:17:15 +0200

    Did extremely important release!

--- src/hotciv/standard/GameImpl.java
+++ src/hotciv/standard/GameImpl.java
@@ -32,6 +32,8 @@ import java.util.Objects;
    See the License for the specific language governing permissions and
    limitations under the License.
 
+ This is an amazing release!?!?!??!
+
 */
 
 public class GameImpl implements Game {
