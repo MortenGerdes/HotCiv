39d724a69d27b8fc07ccc5e1b0c38751b002276a
Morten <mortengerdes7000@gmail.com>
2017-09-17 19:15:21 +0200

    Added a change to master

--- src/hotciv/standard/GameImpl.java
+++ src/hotciv/standard/GameImpl.java
@@ -28,6 +28,7 @@ import java.util.HashMap;
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
+ This is a change
 */
 
 public class GameImpl implements Game {
