1b8d343d18eb59f2e6c7123e00b4ae10cdcfa3fc
Morten <mortengerdes7000@gmail.com>
2017-10-02 16:24:25 +0200

    Fixed some test stup so it would return a fixed value instead of a random.

--- src/hotciv/standard/Strategy/AttackingStrategy/EpsilonCivAttackingStrategy.java
+++ src/hotciv/standard/Strategy/AttackingStrategy/EpsilonCivAttackingStrategy.java
@@ -4,6 +4,7 @@ import hotciv.framework.Game;
 import hotciv.framework.Position;
 import hotciv.framework.Unit;
 import hotciv.standard.GameImpl;
+import hotciv.standard.Utility;
 
 import java.util.HashMap;
 
@@ -12,9 +13,44 @@ import java.util.HashMap;
  */
 public class EpsilonCivAttackingStrategy implements AttackingStrategy{
 
+    private Game game;
+
     @Override
     public HashMap<Position, Unit> attackUnit(Game game, HashMap<Position, Unit> unitHashMap, Position posToMoveFrom, Position posToMoveTo)
     {
-        return null;
+        this.game = game;
+
+        int attackerNumber = calculateStats(game.getUnitAt(posToMoveFrom).getAttackingStrength(), posToMoveFrom);
+        int defenderNumber = calculateStats(game.getUnitAt(posToMoveTo).getDefensiveStrength(), posToMoveTo);
+        int randomValue = 3;
+
+        if(attackerNumber+randomValue > defenderNumber+randomValue)
+        {
+            Unit attackingUnit = game.getUnitAt(posToMoveFrom);
+            unitHashMap.remove(attackingUnit);
+            unitHashMap.put(posToMoveTo, attackingUnit);
+        }
+        else
+        {
+            unitHashMap.remove(game.getUnitAt(posToMoveFrom));
+        }
+
+        return unitHashMap;
+    }
+
+    private int calculateStats(int baseStat, Position center)
+    {
+        int finalStat = baseStat;
+        for(Position adjPos: Utility.get8Neighborhood(center))
+        {
+            if(game.getUnitAt(adjPos) != null)
+            {
+                if(game.getUnitAt(adjPos).getOwner() == game.getUnitAt(center).getOwner())
+                {
+                    finalStat++;
+                }
+            }
+        }
+        return finalStat * Utility.getTerrainFactor(game, center);
     }
 }
--- src/hotciv/standard/Strategy/AttackingStrategy/TestStupAttackingStrategy.java
+++ src/hotciv/standard/Strategy/AttackingStrategy/TestStupAttackingStrategy.java
@@ -3,9 +3,6 @@ package hotciv.standard.Strategy.AttackingStrategy;
 import hotciv.framework.Game;
 import hotciv.framework.Position;
 import hotciv.framework.Unit;
-import hotciv.standard.GameImpl;
-import hotciv.standard.Utility;
-import javafx.geometry.Pos;
 
 import java.util.HashMap;
 
@@ -14,50 +11,10 @@ import java.util.HashMap;
  */
 public class TestStupAttackingStrategy implements AttackingStrategy
 {
-    private Game game;
-    private HashMap<Position, Unit> unitHashMap;
-    private Position posToMoveFrom;
-    private Position posToMoveTo;
 
     @Override
     public HashMap<Position, Unit> attackUnit(Game game, HashMap<Position, Unit> unitHashMap, Position posToMoveFrom, Position posToMoveTo)
     {
-        this.game = game;
-        this.unitHashMap = unitHashMap;
-        this.posToMoveFrom = posToMoveFrom;
-        this.posToMoveTo = posToMoveTo;
-
-        int attackerNumber = calculateStats(game.getUnitAt(posToMoveFrom).getAttackingStrength(), posToMoveFrom);
-        int defenderNumber = calculateStats(game.getUnitAt(posToMoveTo).getDefensiveStrength(), posToMoveTo);
-        int randomValue = 3;
-
-        if(attackerNumber+randomValue > defenderNumber+randomValue)
-        {
-            Unit attackingUnit = game.getUnitAt(posToMoveFrom);
-            unitHashMap.remove(attackingUnit);
-            unitHashMap.put(posToMoveTo, attackingUnit);
-        }
-        else
-        {
-            unitHashMap.remove(game.getUnitAt(posToMoveFrom));
-        }
-
-        return unitHashMap;
-    }
-
-    private int calculateStats(int baseStat, Position center)
-    {
-        int finalStat = baseStat;
-        for(Position adjPos: Utility.get8Neighborhood(center))
-        {
-            if(game.getUnitAt(adjPos) != null)
-            {
-                if(game.getUnitAt(adjPos).getOwner() == game.getUnitAt(center).getOwner())
-                {
-                    finalStat++;
-                }
-            }
-        }
-        return finalStat * Utility.getTerrainFactor(game, center);
+        return null;
     }
 }
